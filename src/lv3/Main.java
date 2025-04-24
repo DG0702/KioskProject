3097535a0b4d5269c02927c10b70e784abea8676
DongGeun <kk506kk@naver.com>
2025-04-24 14:41:13 +0900

    LV 3

--- src/lv3/Main.java
+++ src/lv3/Main.java
@@ -9,10 +9,10 @@ public class Main {
 
         List<MenuItem> menuItems = new ArrayList<>();
 
-        menuItems.add(new lv3.MenuItem("ShackBurger ", 6.9 , " 토마토, 양상추, 쉑소스가 토핑된 치즈버거" ));
-        menuItems.add(new lv3.MenuItem("SmokeShack ", 8.9 , " 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거" ));
-        menuItems.add(new lv3.MenuItem("Cheeseburger ", 6.9 , " 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거" ));
-        menuItems.add(new lv3.MenuItem("Hamburger ", 5.4 , " 비프패티를 기반으로 야채가 들어간 기본버거" ));
+        menuItems.add(new lv3.MenuItem(1,"ShackBurger ", 6.9 , " 토마토, 양상추, 쉑소스가 토핑된 치즈버거" ));
+        menuItems.add(new lv3.MenuItem(2,"SmokeShack ", 8.9 , " 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거" ));
+        menuItems.add(new lv3.MenuItem(3,"Cheeseburger ", 6.9 , " 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거" ));
+        menuItems.add(new lv3.MenuItem(4,"Hamburger ", 5.4 , " 비프패티를 기반으로 야채가 들어간 기본버거" ));
 
         // 키오스크 객체를 인스턴스화
         Kiosk kiosk = new Kiosk(menuItems);
--- src/lv3/MenuItem.java
+++ src/lv3/MenuItem.java
@@ -3,20 +3,24 @@ package lv3;
 public class MenuItem {
 
     // 속성
+    private int number;
     private String hamburgerName;
     private double price;
     private String description;
 
     // 생성자
-    MenuItem(String hamburgerName, double price, String description) {
+    MenuItem(int number,String hamburgerName, double price, String description) {
+        this.number = number;
         this.hamburgerName = hamburgerName;
         this.price = price;
         this.description = description;
     }
 
-    // 속성
 
     //getter
+    public int getNumber() {
+        return number;
+    }
 
     public String getHamburgerName() {
         return hamburgerName;
@@ -30,10 +34,19 @@ public class MenuItem {
         return description;
     }
 
+    // 메뉴 출력
     public void getMenuItem(){
         System.out.println("햄버거 = " + getHamburgerName());
         System.out.println("가격 = " + getPrice());
         System.out.println("설명 = " + getDescription());
+    }
+
+    // 메뉴 설명
+    public String getMenu (){
+        return  + getNumber() + "번 " +
+                "햄버거 : " + getHamburgerName() +
+                " 가격 : " + getPrice() +
+                " 설명 :" +getDescription();
     }
 
 }
