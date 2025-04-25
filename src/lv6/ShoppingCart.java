8395d3779916c658c3fd0d12fb7e31023e6c4f6c
DongGeun <kk506kk@naver.com>
2025-04-25 16:44:42 +0900

    LV 6

--- src/lv6/ShoppingCart.java
+++ src/lv6/ShoppingCart.java
@@ -3,7 +3,6 @@ package lv6;
 
 public class ShoppingCart  {
     // 속성
-
     private MenuItem menu;
     private double sum;
 
@@ -12,29 +11,26 @@ public class ShoppingCart  {
         this.menu = menu;
     }
 
-
+    // 장바구니에 담긴 목록 이름
     public String getMenuName(){
         return menu.getMenuName();
     }
 
+    // 장바구니에 담긴 목록 가격
     public double getMenuPrice(){
         return menu.getMenuPrice();
     }
 
+    // 장바구니에 담긴 목록 설명 
     public String getMenuDescription(){
         return menu.getMenuDescription();
     }
 
-
+    // 장바구니 주문 목록
     public String getMenu (){
         return "주문목록 : " + getMenuName() + "  |  가격 : " + getMenuPrice() + "    | 설명 : " + getMenuDescription();
     }
-
-    // 모든 메뉴 가격의 합
-    public double getSumPrice(){
-        sum += menu.getMenuPrice();
-        return sum;
-    }
+    
 
 
 
