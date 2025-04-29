a8bf0482e232997ce9225614b8762d6b83e2c876
DongGeun <kk506kk@naver.com>
2025-04-29 20:45:38 +0900

    LV 3 리팩토링 : import 정리

--- src/lv3/Main.java
+++ src/lv3/Main.java
@@ -26,9 +26,9 @@ public class Main {
     // 메뉴 추가하는 메서드
     private static void initializeMenu(){
         // 메뉴 추가
-        menuItems.add(new lv3.MenuItem(1,"ShackBurger ", 6.9 , " 토마토, 양상추, 쉑소스가 토핑된 치즈버거" ));
-        menuItems.add(new lv3.MenuItem(2,"SmokeShack ", 8.9 , " 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거" ));
-        menuItems.add(new lv3.MenuItem(3,"Cheeseburger ", 6.9 , " 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거" ));
-        menuItems.add(new lv3.MenuItem(4,"Hamburger ", 5.4 , " 비프패티를 기반으로 야채가 들어간 기본버거" ));
+        menuItems.add(new MenuItem(1,"ShackBurger ", 6.9 , " 토마토, 양상추, 쉑소스가 토핑된 치즈버거" ));
+        menuItems.add(new MenuItem(2,"SmokeShack ", 8.9 , " 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거" ));
+        menuItems.add(new MenuItem(3,"Cheeseburger ", 6.9 , " 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거" ));
+        menuItems.add(new MenuItem(4,"Hamburger ", 5.4 , " 비프패티를 기반으로 야채가 들어간 기본버거" ));
     }
 }
