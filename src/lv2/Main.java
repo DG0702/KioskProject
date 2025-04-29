6c79ed8ca9bc899d6615719ade8840785dd6e187
DongGeun <kk506kk@naver.com>
2025-04-29 14:13:01 +0900

    LV2 리팩토링 : 메서드 분리, 가독성 향상

--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -1,6 +1,5 @@
 package lv2;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -11,6 +10,8 @@ public class Main {
     // 메뉴 추가 리스트 생성
     static List<MenuItem> menuItems = new ArrayList<>();
 
+    
+    // main 메서드
     public static void main(String[] args) {
 
         // Scanner 선언
@@ -29,21 +30,22 @@ public class Main {
 
                 // 메뉴 번호
                 choice = sc.nextInt();
-                
-                // 메뉴 선택 시
-                if(choice < menuItems.size()){
-                    System.out.println(menuItems.get(choice-1).getMenu());
-                    break;
+
+                // 메뉴 번호를 선택하지 않았을 경우
+                if(choice > menuItems.size()){
+                    System.out.println("메뉴 번호를 선택해주세요 (1,2,3,4)");
+                    continue;
                 }
                 
-                // 메뉴 번호를 선택하지 않았을 경우
-                System.out.println("메뉴 번호를 선택해주세요 (1,2,3,4)");
+                // 선택한 메뉴 출력 
+                choiceMenu(choice);
 
             } while (choice != 0);
         } catch (InputMismatchException e) {
             throw new InputMismatchException("메뉴번호를 선택해주세요");
         }
     }
+
 
 
     // 메뉴 출력 메서드
@@ -65,6 +67,12 @@ public class Main {
         menuItems.add(new MenuItem(2,"SmokeShack ", 8.9 , " 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거" ));
         menuItems.add(new MenuItem(3,"Cheeseburger ", 6.9 , " 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거" ));
         menuItems.add(new MenuItem(4,"Hamburger ", 5.4 , " 비프패티를 기반으로 야채가 들어간 기본버거" ));
+    }
+
+    // 선택한 메뉴 출력 메서드 
+    private static void choiceMenu(int choice){
+        // 메뉴 선택
+        System.out.println(menuItems.get(choice-1).getMenu());
     }
 }
 
